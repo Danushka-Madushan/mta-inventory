e6bff10b25eed97951e17bc86db6ca0a288c9f46
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-01-10 11:35:37 +0530

    code cleanup

---
+++ js/resolve.js
@@ -0,0 +1,13 @@
+const Icons = {
+    'Item' : 'fa-solid fa-box',
+    'Burger' : 'fa-solid fa-burger',
+    'Water' : 'fa-solid fa-droplet',
+    '.50 Ammo' : 'fa-solid fa-gun',
+    'Bandage' : 'fa-solid fa-bandage',
+    'Fuel Can' : 'fa-solid fa-gas-pump',
+}
+
+const maxAmount = {
+    'a' : 10,
+    'b' : 5,
+}
